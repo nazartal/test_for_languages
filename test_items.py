0f99b28500f3fe90b62b12a42b84beb23b5ad38e
nazartal <nazartal@yandex.ru>
2019-09-24 21:35:55 +0700

    ДОбавил файл с тестом и файл настроек

---
+++ test_items.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def add_in_basket_button_present(browser):
+    try:
+        browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    except (NoSuchElementException):
+        return False
+    return True
+
+def test_add_in_basket_button(browser):
+    browser.get(link)
+    assert add_in_basket_button_present(browser), "Кнопка добавления в корзину не найдена"
+    time.sleep(30)
