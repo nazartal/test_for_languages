b295d81093f896d2b7b7c66ffc57c36639c29823
nazartal <35163740+nazartal@users.noreply.github.com>
2019-09-24 23:28:23 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,17 +1,12 @@
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def basket_button_present(browser):
-    try:
-        browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
-    except (NoSuchElementException):
-        return False
-    return True
-
-def test_add_in_basket_button(browser):
+def test_check_for_add_button(browser):
     browser.get(link)
-    assert add_in_basket_button_present(browser), "Кнопка добавления в корзину не найдена"
-    time.sleep(30)
+    time.sleep(10)
+    try:
+    	button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    except Exception:
+    	button = False
+    assert button, "Кнопка добавления в корзину не найдена"
